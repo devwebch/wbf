c9f561a8cc744a924bad622ab13de077dbf17148
Simon Rapin <simon.rapin@gmail.com>
2016-08-10 21:38:18 +0200

    added notes to leads table

---
+++ database/migrations/2016_08_10_192809_add_notes_to_leads.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNotesToLeads extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('leads', function (Blueprint $table) {
+            $table->text('notes');
+            $table->tinyInteger('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('leads', function (Blueprint $table) {
+            //
+        });
+    }
+}
