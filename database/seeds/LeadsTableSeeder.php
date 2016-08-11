a6ed1285eae0fe1d93d967edf0ca09eb316a60d6
Simon Rapin <srapin@novahealthsa.com>
2016-08-11 16:14:41 +0200

    updated Lead table seeder

--- database/seeds/LeadsTableSeeder.php
+++ database/seeds/LeadsTableSeeder.php
@@ -14,11 +14,14 @@ class LeadsTableSeeder extends Seeder
         for ($i = 0; $i < 10; $i++) {
             DB::table('leads')->insert(
                 [
-                    'name'      => 'Shop ' . str_random(5),
-                    'url'       => 'http://example.com',
-                    'address'   => 'Rue de Genève 7, 1003 Lausanne',
-                    'lat'       => 46.5215533,
-                    'lng'       => 6.6287104
+                    'name'          => 'Shop ' . str_random(5),
+                    'url'           => 'http://example.com',
+                    'address'       => 'Rue de Genève 7, 1003 Lausanne',
+                    'lat'           => 46.5215533,
+                    'lng'           => 6.6287104,
+                    'user_id'       => 0,
+                    'created_at'    => date('Y-m-d H:i:s'),
+                    'updated_at'    => date('Y-m-d H:i:s')
                 ]
             );
         }
