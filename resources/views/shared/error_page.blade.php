8e5a262c8c6bac782d0cdccac3d198d20b5048b9
Simon Rapin <srapin@novahealthsa.com>
2016-08-17 16:30:25 +0200

    new generic error page

---
+++ resources/views/shared/error_page.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', 'Generic error page')
+
+@section('styles')
+@endsection
+
+@section('scripts')
+@endsection
+
+@section('content')
+<h1>Oops something went wrong.</h1>
+@endsection
